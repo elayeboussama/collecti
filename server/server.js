1a56c085bca5e7467aaec3f90a1424512aa140f6
oussama <elayeb.oussama2020@gmail.com>
2022-11-14 09:07:33 +0100

    correction routes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 
-const adminRoutes = require("./routes/AdminControler");
+const adminRoutes = require("./routes/AdminController");
 
 
 
