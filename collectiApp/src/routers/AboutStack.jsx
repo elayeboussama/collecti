8fa91585e115fd7fb9cad32ca887326b8397b88f
oussama <elayeb.oussama2020@gmail.com>
2022-11-16 18:46:36 +0100

    drawer

--- collectiApp/src/routers/AboutStack.jsx
+++ collectiApp/src/routers/AboutStack.jsx
@@ -2,15 +2,35 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import { Header } from "../components";
 import * as views from "../views";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 // home stack navigator screens
 const AboutStack = () => {
+
   return (
     <Stack.Navigator
-      screenOptions={({ navigation }) => {
-        header: () => <Header title="Collecti" navigation={navigation} />;
-      }}
+      screenOptions={ ({ navigation, route }) => ({
+        headerLeft: () => (
+          <MaterialIcons
+            name="menu"
+            size={28}
+            onPress={()=> navigation.openDrawer()}
+            style={{position: "absolute", left: 16, color:"white" }}
+          />
+        ),
+        title: 'My home',
+        headerStyle: {
+          backgroundColor: '#432C7A',
+        },
+        headerTitleAlign:"center",
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        // Add a placeholder button without the `onPress` to avoid flicker
+        
+      })}
     >
       <Stack.Screen
         name="About"
--- collectiApp/src/views/AboutScene/styles.js
+++ collectiApp/src/views/AboutScene/styles.js
@@ -1,12 +1,12 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
-export {styles}
+export { styles }
