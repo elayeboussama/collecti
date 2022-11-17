de4ca54b322d3ba997fb7dd19cd959590dd77793
Ghazi-Chaftar <89045357+Ghazi-Chaftar@users.noreply.github.com>
2022-11-17 22:22:01 +0100

    login signup

--- collectiApp/src/views/index.js
+++ collectiApp/src/views/index.js
@@ -1,4 +1,4 @@
 import HomeScene from "./HomeScene/HomeScene";
 import AboutScene from "./AboutScene/AboutScene";
-
-export { HomeScene,AboutScene }
+import AuthScene from "./AuthScene/AuthScene";
+export { HomeScene,AboutScene,AuthScene }
