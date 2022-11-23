8b7800a87d10b23cf59d3863189735af3733e9b5
oussama <elayeb.oussama2020@gmail.com>
2022-11-23 10:46:33 +0100

    back changes

--- collectiApp/src/components/EventCard/styles.js
+++ collectiApp/src/components/EventCard/styles.js
@@ -1,64 +1,64 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    cardContainer:{
-        marginTop:12,
-        elevation:3,
-        borderRadius:12,
-        width:"95%",
-        alignSelf:"center",
-        
-        display :"flex",
-        flexDirection:"column",
-        padding:8,
+    cardContainer: {
+        marginTop: 12,
+        elevation: 3,
+        borderRadius: 12,
+        width: "95%",
+        alignSelf: "center",
+        backgroundColor: "#fff",
+        display: "flex",
+        flexDirection: "column",
+        padding: 8,
     },
-    headerSection:{
-        flexDirection:"row",
-        gap:8,
-        alignItems:"center",
-        height:50,
+    headerSection: {
+        flexDirection: "row",
+        gap: 8,
+        alignItems: "center",
+        height: 50,
     },
-    leftSection:{
-            
+    leftSection: {
+
     },
-    rightSection:{
-       
+    rightSection: {
+
     },
-    chips:{
-        marginTop:12,
-        marginBottom:12,
-        display:"flex",
-        justifyContent : "center",
-        flexDirection:"row",
-        flexWrap:"wrap",
+    chips: {
+        marginTop: 12,
+        marginBottom: 12,
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "row",
+        flexWrap: "wrap",
     },
-    eventName:{
-        fontSize:20,
-        fontWeight:"bold",
+    eventName: {
+        fontSize: 20,
+        fontWeight: "bold",
     },
-    actions:{
-        marginTop:12,
-        flexDirection:"row",
-        width:"100%",
-        
+    actions: {
+        marginTop: 12,
+        flexDirection: "row",
+        width: "100%",
+
         height: 30,
-        alignItems:"stretch"
+        alignItems: "stretch"
 
     },
-    actionLeft:{
-        width:"50%",
-        justifyContent:"center",
-        alignItems:"center",
-        flexDirection:"row",
+    actionLeft: {
+        width: "50%",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
 
     },
-    actionRight:{
-        borderWidth:1,
-        flexDirection:"row",
-        width:"50%",
-        justifyContent:"center",
-        alignItems:"center",
+    actionRight: {
+        borderWidth: 1,
+        flexDirection: "row",
+        width: "50%",
+        justifyContent: "center",
+        alignItems: "center",
     }
-  });
+});
 
-export {styles}
+export { styles }
