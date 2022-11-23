66a0ddeaf27dd07f708894607ba85a21a4a8590b
Storm <ferjani.nader@hotmail.fr>
2022-11-23 19:23:07 +0100

    added a model component

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import router from './routes';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
