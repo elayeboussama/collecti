421320a97db8c400144242f054e91d26dfd6f7cd
Storm <ferjani.nader@hotmail.fr>
2022-11-23 20:17:20 +0100

    added oraganization card ui

---
+++ client/src/components/OrganizationCard.jsx
@@ -0,0 +1,25 @@
+const OrganizationCard = () => {
+    return (
+        <div className="relative card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://media.slidesgo.com/storage/24722876/lung-cancer-awareness-month1663157891.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">
+                    Organization name
+                    <div className="badge badge-secondary">3</div>
+                </h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi ipsam error harum distinctio ducimus esse debitis dignissimos cumque iusto cupiditate.</p>
+                <div className="flex justify-between items-center">
+                    <div className="card-actions">
+                        <div className="badge badge-outline">Cancer</div>
+                        <div className="badge badge-outline">Health</div>
+                    </div>
+                    <div>
+                        <button className="link btn-sm hover:text-primary">Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OrganizationCard
