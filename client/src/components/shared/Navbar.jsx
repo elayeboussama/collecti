b2cd6b10abc5d3f69208f3956ced6031bb3e1ec2
Storm <ferjani.nader@hotmail.fr>
2022-11-24 14:16:21 +0100

    add search input

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -1,6 +1,13 @@
+import { Bars3BottomLeftIcon } from "@heroicons/react/24/outline"
+import SearchInput from "./SearchInput"
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className="flex p-2 justify-between w-full">
+            <label htmlFor="my-drawer-2" className="btn btn-square btn-ghost drawer-button lg:hidden"><Bars3BottomLeftIcon className="h-6 w-6" /></label>
+
+            <SearchInput />
+        </div>
     )
 }
 
