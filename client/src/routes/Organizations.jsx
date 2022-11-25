d3a88b825063d9f6b7dc0e56a34b5785be061b83
Storm <ferjani.nader@hotmail.fr>
2022-11-25 12:45:34 +0100

    added drawer ui

--- client/src/routes/Organizations.jsx
+++ client/src/routes/Organizations.jsx
@@ -3,7 +3,7 @@ import OrganizationCard from "../components/OrganizationCard"
 const Organizations = () => {
     return (
         <div className="flex items-center justify-center pt-4 sm:p-4">
-            <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 flex-shrink-[2]">
+            <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4">
                 <OrganizationCard />
                 <OrganizationCard />
                 <OrganizationCard />
