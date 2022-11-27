b34685dc97c4cc58bd6b420cfe2302e685b8c392
Storm <ferjani.nader@hotmail.fr>
2022-11-27 16:04:25 +0100

    added login to modal

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,9 @@
+import { XMarkIcon } from "@heroicons/react/24/outline"
+
 const Login = () => {
     return (
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div className="card flex-shrink-0 mx-auto max-w-sm">
+            <label htmlFor="my-modal-4" className="btn btn-circle btn-ghost absolute right-2 top-2"><XMarkIcon className="h-6 w-6" /></label>
             <div className="card-body">
                 <div className="form-control">
                     <label className="label">
@@ -20,9 +23,15 @@ const Login = () => {
                 <div className="form-control mt-6">
                     <button className="btn btn-primary">Login</button>
                 </div>
+                <div className="flex text-sm">
+                    <p className="text-base-content/70 grow-0 mr-1">Already have an account?</p><a className="link link-hover">Sign up</a>
+                </div>
             </div>
         </div>
     )
 }
 
 export default Login
+
+// Todo: add logo on top of the modal
+// Todo: fix input on focus when card is clicked
