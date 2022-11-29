66b5bd6729901b81b782036a65b74b46f8f2b315
Sirine <sirinebenabdallah092@gmail.com>
2022-11-30 00:08:13 +0100

    events-list

--- client/src/routes/Events.jsx
+++ client/src/routes/Events.jsx
@@ -1,6 +1,52 @@
+import EventCard from "../components/event/EventCard"
+import Tabs from "../components/shared/Tab/Tab"
+
+
 const Events = () => {
+    const tabsTitle=["Computer science", "Robotics", "Cultural"]
+    const csEvents=[
+        {
+            id:1,
+            eventTitle: "event1"
+        },
+        {
+            id:2,
+            eventTitle: "event2"
+        },
+        {
+            id:3,
+            eventTitle: "event3"
+        },
+        {
+            id:4,
+            eventTitle: "event4"
+        },
+    ]
     return (
-        <div>Events</div>
+        <div>
+          
+           <Tabs 
+           tabsTitle={tabsTitle}
+           content1={<div className="flex items-center justify-center py-4 sm:p-4 h-full">
+             <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-7 h-full">
+                        {csEvents.map((csevent,i)=>
+                            <EventCard eventTitle={csevent.eventTitle}/>
+                            
+
+                            )}
+                    </div>
+                    </div>}
+                    
+                        
+                    
+            content2={
+                <EventCard eventTitle="robot event"/>
+                    }
+            content3={
+                <EventCard eventTitle="cultural event"/>
+                    }
+           />
+        </div>
     )
 }
 
