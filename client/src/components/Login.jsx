47e3f57a8bd6eb39e5b29ed0593b2f95fed94aa4
Storm <ferjani.nader@hotmail.fr>
2022-12-04 09:44:38 +0100

    added eye icon for hide/show password

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,46 +1,57 @@
-import { XMarkIcon } from "@heroicons/react/24/outline"
+import { EyeIcon, EyeSlashIcon, XMarkIcon } from "@heroicons/react/24/outline"
+import { useState } from "react";
 import { useDispatch } from "react-redux"
 import { setContent } from "../features/modalSlice"
 import Register from "./Register"
 
+
 const Login = () => {
     const dispatch = useDispatch()
+    const [showPassword, setShowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
 
     return (
-        <label className="modal-box  max-w-sm lg:max-w-2xl relative bg-base-100 p-0">
-            <div className="card flex-shrink-0 mx-auto">
-                <label htmlFor="my-modal-4" className="btn btn-circle btn-ghost absolute right-2 top-2"><XMarkIcon className="h-6 w-6" /></label>
-                <div className="flex flex-row p-8 justify-around items-center">
+        <label className="relative max-w-sm p-0 modal-box lg:max-w-2xl bg-base-100">
+            <div className="flex-shrink-0 mx-auto card">
+                <label htmlFor="my-modal-4" className="absolute btn btn-circle btn-ghost right-2 top-2"><XMarkIcon className="w-6 h-6" /></label>
+                <div className="flex flex-row items-center justify-around p-8">
                     <img className="hidden lg:block max-h-80" src="https://svgshare.com/i/oTj.svg" alt="login" />
-                    <div className="card-body p-0 flex-grow max-w-sm px-2">
+                    <div className="flex-grow max-w-sm p-0 px-2 card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
                             <input type="text" placeholder="email" className="input input-bordered" />
                         </div>
-                        <div className="form-control">
+                        <label className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <div className="relative w-full">
+                                <input type={showPassword ? "text" : "password"} placeholder="password" className="w-full input input-bordered" />
+                                <button onClick={toggleShowPassword} className="absolute right-0 btn btn-ghost">
+                                    {showPassword ? <EyeSlashIcon className="w-6 h-6" /> : <EyeIcon className="w-6 h-6" />}
+                                </button>
+                            </div>
                             <label className="label">
                                 <span className="label-text-alt link link-hover">Forgot password?</span>
                             </label>
-                        </div>
-                        <div className="form-control mt-6">
+                        </label>
+                        <div className="mt-6 form-control">
                             <button className="btn btn-primary">Login</button>
                         </div>
                         <div className="flex text-sm">
-                            <p className="text-base-content/70 grow-0 mr-1">Not yet registered?</p><span onClick={() => dispatch(setContent(<Register />))} className="link link-hover">Sign up</span>
+                            <p className="mr-1 text-base-content/70 grow-0">Not yet registered?</p><span onClick={() => dispatch(setContent(<Register />))} className="link link-hover">Sign up</span>
                         </div>
                     </div>
                 </div>
             </div>
         </label>
-
-    )
-}
+    );
+};
 
 export default Login
 
