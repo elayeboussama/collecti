3d189fe3c9192ca19cf5f5da0d1193f58d6dba26
oussama <elayeb.oussama2020@gmail.com>
2023-01-01 16:29:12 +0100

    organization duplicated resolved

--- server/routes/OrganizationController.js
+++ server/routes/OrganizationController.js
@@ -75,12 +75,19 @@ router.post("/login", async (req, res) => {
 
 router.post("/create", async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(Number(process.env.SALT));
-    console.log(req.body);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-    await new Organization({ ...req.body, password: hashPassword }).save();
-    res.status(201).send({ message: "Organization created successfully" });
+    const org = Organization.find({email: req.body.email})
+
+    if(org!= undefined){
+      const salt = await bcrypt.genSalt(Number(process.env.SALT));
+      console.log(req.body);
+      const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+      await new Organization({ ...req.body, password: hashPassword }).save();
+      res.status(201).send({ message: "Organization created successfully" });
+    }else{
+      res.status(420).send({ message: "Organization already exist" });
+    }
   } catch (error) {
     res.status(500).send({ message: "Internal Server Error", error: error });
     console.log(error);
