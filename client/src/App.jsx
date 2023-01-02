62ec842b2cc70b491ee40c3e6027677b8924ba98
Storm <ferjani.nader@hotmail.fr>
2023-01-02 08:11:39 +0100

    added toggle between user logged in/out in drawer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,18 @@ import Footer from "./components/shared/Footer";
 import Navbar from "./components/shared/Navbar";
 import Drawer from "./components/shared/Drawer";
 import Modal from "./components/shared/Modal";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { checkToken } from "./features/authSlice";
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkToken());
+  }, [dispatch]);
+
   return (
     <div>
       <Drawer>
