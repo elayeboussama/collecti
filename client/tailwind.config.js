7b1885cc504cc5160acef981edca06d04e08d407
Sirine <sirinebenabdallah092@gmail.com>
2023-01-02 12:44:22 +0100

    home page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        l_blue: '#EFF5F9',
+        d_blue: '#75BB99',
+        d_green: '#003E1C',
+
+      },
+   
+    },
   },
   plugins: [require("daisyui")],
 }
