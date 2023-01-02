1a245d1d6f41c28117bae2dc5a396303201e5823
Storm <ferjani.nader@hotmail.fr>
2023-01-02 21:02:19 +0100

    fixed image upload component

--- client/src/components/ImageUpload.jsx
+++ client/src/components/ImageUpload.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from 'react';
-
-// Import React FilePond
+import React from 'react';
 import { FilePond, registerPlugin } from 'react-filepond';
-
 import 'filepond/dist/filepond.min.css';
-
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
-import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview, FilePondPluginFileValidateType);
@@ -15,18 +11,16 @@ registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview, F
 
 const ImageUpload = ({ images, setImages }) => {
     return (
-        <div className="App">
-            <FilePond
-                files={images}
-                onupdatefiles={setImages}
-                allowMultiple={true}
-                maxFiles={10}
-                instantUpload={false}
-                name="files"
-                acceptedFileTypes={['image/*']}
-                labelIdle='Got some great images to share? 📸 drag & drop them or <span class="filepond--label-action">Browse</span>'
-            />
-        </div>
+        <FilePond
+            files={images}
+            onupdatefiles={setImages}
+            allowMultiple={true}
+            maxFiles={10}
+            instantUpload={false}
+            name="files"
+            acceptedFileTypes={['image/*']}
+            labelIdle='Got some great images to share? 📸 drag & drop them or <span class="filepond--label-action">Browse</span>'
+        />
     );
 }
 
