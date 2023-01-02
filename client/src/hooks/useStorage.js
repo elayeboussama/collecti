171c9a688ac86ee0a9187fc26c8de719ba04cf1e
Storm <ferjani.nader@hotmail.fr>
2023-01-02 12:43:36 +0100

    added useStorage hook

--- client/src/hooks/useStorage.js
+++ client/src/hooks/useStorage.js
@@ -1,33 +1,28 @@
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { useState } from "react";
+import { v4 } from 'uuid';
 import { storage } from "../firebase";
-import { useId } from 'react';
 
 
-export const useStorage = (file) => {
-    const [progress, setProgress] = useState(0);
+export const useStorage = () => {
     const [error, setError] = useState(null);
     const [url, setUrl] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
-    const id = useId();
-
-    const uploadFile = (file) => {
-        const storageRef = storage.ref(file.name + id);
-        storageRef.put(file).on(
-            "state_changed",
-            (snap) => {
-                let percentage = Math.round((snap.bytesTransferred / snap.totalBytes) * 100);
-                setProgress(percentage);
-            },
-            (err) => {
-                setError(err);
-            },
-            async () => {
-                const downloadUrl = await storageRef.getDownloadURL();
-                setUrl(downloadUrl);
-            }
-        );
+    const uploadFile = async (file) => {
+        setIsLoading(true);
+        try {
+            const storageRef = ref(storage, file.name + v4());
+            const response = await uploadBytes(storageRef, file)
+            await getDownloadURL(response.ref).then((downloadedURL) => {
+                setUrl(downloadedURL);
+            })
+        } catch (err) {
+            setError(err);
+        }
+        setIsLoading(false);
     }
 
-    return { progress, url, error, uploadFile };
+    return { url, error, uploadFile, isLoading };
 
 };
