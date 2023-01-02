e4f7d538185f6eb5dd3f8271b87aa8c82ddaa906
Sirine <sirinebenabdallah092@gmail.com>
2023-01-02 16:19:10 +0100

    final home

---
+++ client/src/routes/JoinUsSection.jsx
@@ -0,0 +1,19 @@
+import Register from "../components/Register"
+
+const JoinUsSection = ()=>{
+    return(
+        <div className="hero py-28" id="login">
+            <div className="flex-col pr-10 hero-content lg:flex-row-reverse pl-28">
+                <div className="w-11/12 pl-24 text-center lg:text-left">
+                <h1 className="text-5xl font-bold">Join us Now!</h1>
+                <p className="py-6">Provident cupiditate voluptatem et in.
+                 Quaerat fugiat ut assumenda excepturi exercitationem quasi.
+                  In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <Register/>
+            </div>
+            </div>
+
+    )
+}
+export default JoinUsSection
