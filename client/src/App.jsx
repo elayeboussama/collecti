b3212c895c6931e7924add5356afd98182ff5fa2
Sirine <sirinebenabdallah092@gmail.com>
2023-01-02 12:44:22 +0100

    home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import Footer from "./components/shared/Footer";
 import Navbar from "./components/shared/Navbar";
 import Drawer from "./components/shared/Drawer";
 import Modal from "./components/shared/Modal";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
       <Drawer>
-        <Navbar />
+        {/* <Navbar /> */}
+        {/* <Home/> */}
         <Outlet />
         <Footer />
       </Drawer>
