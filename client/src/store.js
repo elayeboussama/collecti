ed0fb39c9cf7d518397471b2c3e358cfe64c5d8d
Storm <ferjani.nader@hotmail.fr>
2023-01-15 16:42:18 +0100

    added conffeti

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { apiSlice } from './features/apiSlice'
+import { apiSlice, apiSliceWithToken } from './features/apiSlice'
 import authReducer from './features/authSlice'
 import modalReducer from './features/modalSlice'
+import ConffetiReducer from './features/conffetiSlice'
 
 export const store = configureStore({
     reducer: {
         modal: modalReducer,
         [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authReducer
+        [apiSliceWithToken.reducerPath]: apiSliceWithToken.reducer,
+        auth: authReducer,
+        conffeti: ConffetiReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
-    }).concat(apiSlice.middleware)
+    }).concat(apiSlice.middleware, apiSliceWithToken.middleware)
 })
