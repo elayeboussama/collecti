002baac6c8010ae6490ca5e9ae4f5e0dcc57d2c7
Sirine <sirinebenabdallah092@gmail.com>
2023-01-18 23:10:30 +0100

    stripe

---
+++ client/src/components/event/PayButton.jsx
@@ -0,0 +1,33 @@
+import { useDispatch } from "react-redux"
+import { usePaymentMutation } from "../../endpoints/AuthEndpoints"
+
+
+const PayButton = ({onClose, informations}) =>{
+    const dispatch = useDispatch()
+    const [payment, { isLoading: requestLoading }] = usePaymentMutation()
+    const handleCheckOut = async () =>{
+        console.log(informations)
+        try {
+            const response = await payment({ informations }).unwrap()
+            console.log(response)
+            if(response.url){
+                window.location.href=response.url;
+            }
+            // dispatch(setContent(<EventCreatedPopUp url={`http://localhost:3000/events/${response.id}`} id={response.id} />))
+            // dispatch(openModal())
+            // dispatch(setVisible(true))
+            // setTimeout(() => {
+            //     dispatch(setVisible(false))
+            // }, 6000)
+        } catch (error) {
+            console.error(error)
+        }
+
+    }
+    return(
+        <button className="w-1/2 mt-6 btn btn-primary" onClick={()=>handleCheckOut()}>Check out</button>
+    )
+}
+
+
+export default PayButton;
