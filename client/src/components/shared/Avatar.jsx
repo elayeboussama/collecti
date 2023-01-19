ab268d9ccce8f0e2179afc0e77196093e6c693b6
Storm <ferjani.nader@hotmail.fr>
2023-01-19 12:15:54 +0100

    user info are dynamic

--- client/src/components/shared/Avatar.jsx
+++ client/src/components/shared/Avatar.jsx
@@ -1,8 +1,8 @@
-const Avatar = () => {
+const Avatar = ({ src }) => {
     return (
         <div className="avatar">
             <div className="w-14 rounded-3xl">
-                <img src="https://placeimg.com/192/192/people" alt="" />
+                <img src={src || "https://placeimg.com/192/192/people"} alt="" />
             </div>
         </div>
     )
