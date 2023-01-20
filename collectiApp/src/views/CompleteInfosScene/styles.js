bd96195e610d13b30369757dec53498dda1c6352
oussama <elayeb.oussama2020@gmail.com>
2023-01-20 22:57:16 +0100

    app changes

--- collectiApp/src/views/CompleteInfosScene/styles.js
+++ collectiApp/src/views/CompleteInfosScene/styles.js
@@ -3,8 +3,69 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        display: "flex",
+        flexDirection: "column",
+        alignContent: "center",
+        margin: 10,
+        backgroundColor: "#EDEDED",
+        borderRadius: 5,
+        elevation: 3,
     },
+    CoverView: {},
+
+    UpImageBtn: {
+        position: "absolute",
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 130,
+        height: 130,
+        borderRadius: 100,
+        marginTop: 100,
+        marginLeft: 15,
+        elevation: 10,
+        backgroundColor: '#CBCBCB',
+
+    },
+    UpCoverBtn: {
+        position: "absolute",
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 40,
+        height: 40,
+        borderRadius: 100,
+        right: 10,
+        bottom: 10,
+        elevation: 10,
+        backgroundColor: '#CBCBCB',
+    },
+    UpImageUpdate: {
+        position: "absolute",
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 20,
+        height: 20,
+        borderRadius: 100,
+        top: 10,
+        right: 10,
+        elevation: 10,
+        backgroundColor: '#CBCBCB',
+    },
+
+    UpImage: {
+        width: 130,
+        height: 130,
+        borderRadius: 100,
+    },
+
+    UpCover: {
+        width: "100%",
+        height: undefined,
+        aspectRatio: 2,
+        borderRadius: 5,
+        margin: 0
+    },
+
+
     headerSection: {
         marginTop: 12,
         elevation: 3,
@@ -36,7 +97,25 @@ const styles = StyleSheet.create({
         marginBottom: 12,
         flexDirection: "row",
         justifyContent: "center",
+    },
+    backButton: {
+        height: 50,
+        width: 50,
+
+        borderRadius: 30,
+    },
+    backGroundPic: {
+        width: "100%",
+        height: 300,
+
+    },
+    submitButton: {
+        marginTop: 12,
+        width: "80%",
+        alignSelf: "center",
+        borderRadius: 12,
     }
+
 });
 
 export { styles }
