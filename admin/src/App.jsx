9f74509999509fc6c49b6ae070d53fca49b5a180
Storm <ferjani.nader@hotmail.fr>
2023-01-21 03:17:24 +0100

    added and configured daisyui

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <div>
       <h1 className='text-2xl'> Hello Collecti!</h1>
+      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">Button</button>
     </div>
   )
 }
