315b87eb377dcf10b219b854d8fca5ff5267c076
Ghazi-Chaftar <89045357+Ghazi-Chaftar@users.noreply.github.com>
2023-01-22 18:04:25 +0100

    event profile

--- collectiApp/src/views/index.js
+++ collectiApp/src/views/index.js
@@ -8,15 +8,17 @@ import AddEventScene from "./AddEventScene/AddEventScene";
 import CompanySettingsScene from "./CompanySettingsScene/CompanySettingsScene";
 import EditEventScene from "./EditEventScene/EditEventScene";
 import DonateScene from "./DonateScene/DonateScene";
+import EventProfileScene from "./EventProfileScene/EventProfileScene";
 export {
-    HomeScene,
-    AboutScene,
-    AuthScene,
-    CompanySearchScene,
-    CompanyProfileScene,
-    CompleteInfosScene,
-    AddEventScene,
-    CompanySettingsScene,
-    EditEventScene,
-    DonateScene,
+  HomeScene,
+  AboutScene,
+  AuthScene,
+  CompanySearchScene,
+  CompanyProfileScene,
+  CompleteInfosScene,
+  AddEventScene,
+  CompanySettingsScene,
+  EditEventScene,
+  DonateScene,
+  EventProfileScene,
 };
